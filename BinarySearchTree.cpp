408d49403cca2148d4ee4184df64cb85e0d34f76
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-24 10:52:39 +0700

    menetapkan nilai ke dalam node baru

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -34,4 +34,7 @@ class BinaryTree
             //Step 1 : Allocate memory for the new node
             Node *newNode = new Node();
 
-            
+            //Step  2 : Assign value to the data field of the new node
+            newNode->info = x;
+
+           
